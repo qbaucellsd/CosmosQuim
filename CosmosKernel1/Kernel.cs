b977c2204c1b82c19671d7e0efe8b43161b169bd
qbaucellsd <quimbaucellsd@gmail.com>
2024-11-06 14:45:47 +0100

    UpdateGitIgnore

--- CosmosKernel1/Kernel.cs
+++ CosmosKernel1/Kernel.cs
@@ -2,18 +2,27 @@
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
+using Cosmos.System.Graphics;
+using System.Drawing;
+
 
 namespace CosmosKernel1
 {
     public class Kernel : Sys.Kernel
     {
+        //Canvas canvas;
+        Sys.FileSystem.CosmosVFS fs = new Cosmos.System.FileSystem.CosmosVFS();
 
         protected override void BeforeRun()
         {
+
             Sys.KeyboardManager.SetKeyLayout(new Sys.ScanMaps.ESStandardLayout());
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
             Console.Clear();
             Console.WriteLine("           |  | \\     /\\     / |  | Operative\r\n" +
                               "Welcome to |__|   \\_/    \\_/   |__| System\n");
+         //   canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(640, 480, ColorDepth.ColorDepth32));
+         //   canvas.Clear(Color.Blue);
         }
 
         protected override void Run()
@@ -46,12 +55,12 @@ namespace CosmosKernel1
                     Console.WriteLine("The system will restart.");
                     Console.WriteLine("Press enter to confirm...");
                     Console.ReadLine();
-                    Cosmos.System.Power.Reboot();
+                    Sys.Power.Reboot();
                     break;
                 case 4:
                     Console.Clear();
                     Console.WriteLine("The system will shutdown.");
-                    Cosmos.System.Power.Shutdown();
+                    Sys.Power.Shutdown();
                     Console.WriteLine("Press enter to confirm...");
                     Console.ReadLine();
                     break;
